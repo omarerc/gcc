2b78089eb15f3b45958bc3685a83d4febf47d56e
Omar Ramirez <omar_e_rc@hotmail.com>
2023-03-14 19:49:26 -0700

    Comments added and Fixed While Loop in n = 0

--- print-hash.c
+++ print-hash.c
@@ -4,27 +4,35 @@
 
 int main(void) 
 {
-  char *str = NULL;
-  int n;
+  char *str = NULL; // line
+  int n; // n prints
   
   do {
     printf("Enter Value: ");
     scanf("%d", &n);
 
+    // Asign memory
     str = malloc(n * sizeof(char*));
+    // Initiliaze string with blank space
     for (int x = 0; x < n; x++) {
       strcat(str, " ");
     }
 
+    // Check if n is a positive number 
     if (n > 0) {
+      // Print n lines hashes
       while (n > 0) {
         str[n] = '#';
         printf("%s\n", str);
         n--;
       }
+      // Successful exit
+      return 0;
     } else {
       printf("Error: Please insert positive number over 0.\n");
     }
-  } while (n < 0);
- 
+  } while (n <= 0);
+
+  // Error or imposible.
+  return -1;
 }
