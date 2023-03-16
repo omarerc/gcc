86c8b94d4b525f326eb27186233af0284e7e75a0
Omar Ramirez <omar_e_rc@hotmail.com>
2023-03-16 00:32:19 -0700

    Fixed the for loop that print the incoming args.

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 all: binary-tree
 
+#print-hash: print-hash.c
+#	gcc -g print-hash.c -o ./print-hash
+
 binary-tree: binary-tree.c
 	gcc -g binary-tree.c -o ./binary-tree
--- binary-tree.c
+++ binary-tree.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
   printf("Printing Message:\n");
   if (argc > 0)
   {
-    for (int i = 0; i++; i < argc)
+    for (int i = 0; i < argc; i++)
     {
       printf("%s\n", argv[i]);
     }
