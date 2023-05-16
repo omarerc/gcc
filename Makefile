c3299163596a0d8d2481584bdea701f22e21214f
Omar Ramirez <omar_e_rc@hotmail.com>
2023-05-16 12:24:06 -0700

    Struct Contact-Info sample done!

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ all: compile-project
 #print-hash: print-hash.c
 #	gcc -g print-hash.c -o ./print-hash
 
-compile-project: contacts.c
-	gcc -g contacts.c -o ./contacts
+compile-project: contact-struct.c
+	gcc -g contact-struct.c -o ./contact-struct
--- contact-struct.c
+++ contact-struct.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct Date
 {
@@ -8,8 +9,8 @@ struct Date
 
 struct ContactInfo
 {
-  char firstName[20];
-  char lastName[20];
+  char *firstName;
+  char *lastName;
   struct Date dob;
 };
 
@@ -18,8 +19,8 @@ int main()
   struct ContactInfo c1 = 
     { "Omar", "Ramirez", { 1983, 6, 18 } };
 
-  char firstName[20];
-  char lastName[20];
+  char *firstName = malloc(20 * sizeof(char));
+  char *lastName = malloc(20 * sizeof(char));
   int y, m, d;
 
   // Second Contact Scan
@@ -36,6 +37,8 @@ int main()
 
   // Create Contact
   struct ContactInfo c2;
+  c2.firstName = malloc(20 * sizeof(char));
+  c2.lastName = malloc(20 * sizeof(char));
 
   strcpy(c2.firstName, firstName);
   strcpy(c2.lastName, lastName);
@@ -43,10 +46,48 @@ int main()
   c2.dob.m = m;
   c2.dob.d = d;
 
+  free(firstName);
+  free(lastName);
+
+  // Create Contact
+  struct ContactInfo* c3;
+  c3 = malloc(sizeof(struct ContactInfo));
+  c3->firstName = malloc(20 * sizeof(char));
+  c3->lastName = malloc(20 * sizeof(char));
+  strcpy(c3->firstName, "");
+  strcpy(c3->lastName, "");
+
+  while ((getchar()) != '\n');
+
+  // Third Contact Scan
+  printf("First Name: ");
+  scanf("%[^\n]%*c", c3->firstName);
+  printf("Last Name: ");
+  scanf("%[^\n]%*c", c3->lastName);
+  printf("Birth Year: ");
+  scanf("%d", &c3->dob.y);
+  printf("Birth Month: ");
+  scanf("%d", &c3->dob.m);
+  printf("Birth Day: ");
+  scanf("%d", &c3->dob.d);
+
+  // Printing Contact
   printf("Name: %s %s\nBirthday: %d/%d/%d\n",
     c1.firstName, c1.lastName, c1.dob.y, c1.dob.m, c1.dob.d);
 
   printf("Name: %s %s\nBirthday: %d/%d/%d\n",
     c2.firstName, c2.lastName, c2.dob.y, c2.dob.m, c2.dob.d);
+
+  printf("Name: %s %s\nBirthday: %d/%d/%d\n",
+    c3->firstName, c3->lastName, c3->dob.y, c3->dob.m, c3->dob.d);
+ 
+  // Releaseing Dynamic Memory
+  //free(c1.firstName);
+  //free(c1.lastName);
+  //free(c2.firstName);
+  //free(c2.lastName);
+  free(c3->firstName);
+  free(c3->lastName);
+  free(c3);
 
 }
